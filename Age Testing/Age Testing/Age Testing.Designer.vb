2352e2eaf4e74502e790da897cd5ef2f5d055990
JohnPaulSalazar <34857422+JohnPaulSalazar@users.noreply.github.com>
2020-03-06 13:27:19 +0400

    -User Interface Change, added background image -Added a panel named pnlSukatan that serves as basis on other controls layout, and as a courier of results of Age Checking -Age Textbox(Tb1) backcolor change into Peach -Age Textbox(Tb1) borderstyle change into None -Age Textbox(Tb1) size changed -filter the Age textbox, that only accept numeric character -Set the default value of Tb1 to 0, to prevent error when button(Bn1) clicked while textbox(Tb1) value is null -Add a code to prevent flicker when form resize -Change the button layout and set into transparent -Change the buttonBn1 click events, instead of displaying text to textbox(Tb2), message will be shown to to user using a image that shown in the panel(pnlSukatan|) -Delete textbox(Tb2), and label

--- Age Testing/Age Testing/Age Testing.Designer.vb
+++ Age Testing/Age Testing/Age Testing.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,79 +20,60 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Lb1 = New System.Windows.Forms.Label
-        Me.Lb2 = New System.Windows.Forms.Label
-        Me.Tb1 = New System.Windows.Forms.TextBox
-        Me.Tb2 = New System.Windows.Forms.TextBox
-        Me.Bn1 = New System.Windows.Forms.Button
+        Me.Tb1 = New System.Windows.Forms.TextBox()
+        Me.Bn1 = New System.Windows.Forms.Button()
+        Me.pnlSukatan = New System.Windows.Forms.Panel()
         Me.SuspendLayout()
-        '
-        'Lb1
-        '
-        Me.Lb1.AutoSize = True
-        Me.Lb1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Lb1.Location = New System.Drawing.Point(84, 28)
-        Me.Lb1.Name = "Lb1"
-        Me.Lb1.Size = New System.Drawing.Size(127, 25)
-        Me.Lb1.TabIndex = 0
-        Me.Lb1.Text = "Age Tester"
-        '
-        'Lb2
-        '
-        Me.Lb2.AutoSize = True
-        Me.Lb2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Lb2.Location = New System.Drawing.Point(78, 74)
-        Me.Lb2.Name = "Lb2"
-        Me.Lb2.Size = New System.Drawing.Size(98, 16)
-        Me.Lb2.TabIndex = 1
-        Me.Lb2.Text = "Enter Your Age"
         '
         'Tb1
         '
-        Me.Tb1.Location = New System.Drawing.Point(182, 73)
+        Me.Tb1.BackColor = System.Drawing.Color.PeachPuff
+        Me.Tb1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.Tb1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Tb1.Location = New System.Drawing.Point(579, 197)
+        Me.Tb1.Margin = New System.Windows.Forms.Padding(4)
         Me.Tb1.Name = "Tb1"
-        Me.Tb1.Size = New System.Drawing.Size(80, 20)
-        Me.Tb1.TabIndex = 2
-        '
-        'Tb2
-        '
-        Me.Tb2.Enabled = False
-        Me.Tb2.Location = New System.Drawing.Point(81, 160)
-        Me.Tb2.Name = "Tb2"
-        Me.Tb2.Size = New System.Drawing.Size(181, 20)
-        Me.Tb2.TabIndex = 3
+        Me.Tb1.Size = New System.Drawing.Size(105, 91)
+        Me.Tb1.TabIndex = 0
         '
         'Bn1
         '
-        Me.Bn1.Location = New System.Drawing.Point(81, 110)
+        Me.Bn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Bn1.Location = New System.Drawing.Point(108, 135)
+        Me.Bn1.Margin = New System.Windows.Forms.Padding(4)
         Me.Bn1.Name = "Bn1"
-        Me.Bn1.Size = New System.Drawing.Size(181, 32)
+        Me.Bn1.Size = New System.Drawing.Size(241, 39)
         Me.Bn1.TabIndex = 4
-        Me.Bn1.Text = "Get My  Message"
         Me.Bn1.UseVisualStyleBackColor = True
+        '
+        'pnlSukatan
+        '
+        Me.pnlSukatan.BackColor = System.Drawing.Color.Transparent
+        Me.pnlSukatan.Location = New System.Drawing.Point(1, 2)
+        Me.pnlSukatan.Name = "pnlSukatan"
+        Me.pnlSukatan.Size = New System.Drawing.Size(417, 50)
+        Me.pnlSukatan.TabIndex = 6
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(306, 230)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(1060, 585)
         Me.Controls.Add(Me.Bn1)
-        Me.Controls.Add(Me.Tb2)
         Me.Controls.Add(Me.Tb1)
-        Me.Controls.Add(Me.Lb2)
-        Me.Controls.Add(Me.Lb1)
+        Me.Controls.Add(Me.pnlSukatan)
+        Me.DoubleBuffered = True
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "Form1"
         Me.Text = "Age Program"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Lb1 As System.Windows.Forms.Label
-    Friend WithEvents Lb2 As System.Windows.Forms.Label
     Friend WithEvents Tb1 As System.Windows.Forms.TextBox
-    Friend WithEvents Tb2 As System.Windows.Forms.TextBox
     Friend WithEvents Bn1 As System.Windows.Forms.Button
-
+    Friend WithEvents pnlSukatan As Panel
 End Class
--- Age Testing/Age Testing/Age Testing.vb
+++ Age Testing/Age Testing/Age Testing.vb
@@ -1,21 +1,121 @@
 ﻿Public Class Form1
+    'to prevent the Flicker of forms when resizing because of the images
+    Protected Overloads Overrides ReadOnly Property CreateParams() As CreateParams
+        Get
+            Dim cp As CreateParams = MyBase.CreateParams
+            cp.ExStyle = cp.ExStyle Or 33554432
+            Return cp
+        End Get
+    End Property
+
+    'To filter the textbox and accept only numeric character
+    Private Sub Tb1_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles Tb1.KeyPress
+        If e.KeyChar <> ChrW(Keys.Back) Then
+            If Char.IsNumber(e.KeyChar) Then
+            Else
+                e.Handled = True
+            End If
+        End If
+    End Sub
 
     Private Sub Bn1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Bn1.Click
         Dim age As String
-        Dim msg As String
 
         age = Tb1.Text
-        msg = Tb2.Text
 
         If age <= 13 Then
-            Tb2.Text = "you are a child"
+            'If True it will set the background of PnlSukatan to Child Image 
+            pnlSukatan.BackgroundImage = New Bitmap(My.Resources.Child)
+            pnlSukatan.BackgroundImageLayout = ImageLayout.Stretch
         ElseIf age <= 21 Then
-            Tb2.Text = "you are a teenager"
+            'If True it will set the background of PnlSukatan to TeenAger Image
+            pnlSukatan.BackgroundImage = New Bitmap(My.Resources.Teenager)
+            pnlSukatan.BackgroundImageLayout = ImageLayout.Stretch
         ElseIf age <= 65 Then
-            Tb2.Text = "you are an adult"
+            'If True it will set the background of PnlSukatan to Adult Image
+            pnlSukatan.BackgroundImage = New Bitmap(My.Resources.Adult)
+            pnlSukatan.BackgroundImageLayout = ImageLayout.Stretch
         ElseIf age > 65 Then
-            Tb2.Text = "you are old"
+            'If True it will set the background of PnlSukatan to Old Image
+            pnlSukatan.BackgroundImage = New Bitmap(My.Resources.Old)
+            pnlSukatan.BackgroundImageLayout = ImageLayout.Stretch
         End If
 
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Set background image to enhance User Interface
+        Me.BackgroundImage = New Bitmap(My.Resources.Background)
+        Me.BackgroundImageLayout = ImageLayout.Stretch
+
+        'To Fill the Form with panel, it serve as basis of other controls
+        pnlSukatan.Dock = DockStyle.Fill
+
+        'To layout the position and size of the button Check
+        Bn1.Width = pnlSukatan.Width / 2.65
+        Bn1.Height = pnlSukatan.Height / 8
+        Bn1.Left = pnlSukatan.Left + pnlSukatan.Width / 1.85
+        Bn1.Top = pnlSukatan.Top + pnlSukatan.Height / 1.8
+
+        'To Set the Button Into Transparent
+        Bn1.FlatStyle = FlatStyle.Flat
+        Bn1.FlatAppearance.BorderSize = 0
+        Bn1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Bn1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Bn1.BackColor = System.Drawing.Color.Transparent
+
+        'To Set all the Input text or numbers unto the center  
+        Tb1.TextAlign = HorizontalAlignment.Center
+        'It makes the Textbox Layout works perfectly
+        Tb1.Multiline = True
+        'To Set Value on textbox on load to avoid error if Button is clicked while textbox is empty
+        Tb1.Text = 0
+
+        'To layout the position and size of the TextBox Check
+        Tb1.Width = pnlSukatan.Width / 2.65
+        Tb1.Height = pnlSukatan.Height / 6
+        Tb1.Left = pnlSukatan.Left + pnlSukatan.Width / 1.85
+        Tb1.Top = pnlSukatan.Top + pnlSukatan.Height / 2.9
+
+
+    End Sub
+
+
+    'To reset the control layout when the Window Size change
+    Private Sub Form1_ResizeEnd(sender As Object, e As EventArgs) Handles Me.ResizeEnd
+        'Set background image to enhance User Interface
+        Me.BackgroundImage = New Bitmap(My.Resources.Background)
+        Me.BackgroundImageLayout = ImageLayout.Stretch
+
+        'To Fill the Form with panel, it serve as basis of other controls
+        pnlSukatan.Dock = DockStyle.Fill
+
+        'To layout the position and size of the button Check
+        Bn1.Width = pnlSukatan.Width / 2.65
+        Bn1.Height = pnlSukatan.Height / 8
+        Bn1.Left = pnlSukatan.Left + pnlSukatan.Width / 1.85
+        Bn1.Top = pnlSukatan.Top + pnlSukatan.Height / 1.8
+
+        'To Set the Button Into Transparent
+        Bn1.FlatStyle = FlatStyle.Flat
+        Bn1.FlatAppearance.BorderSize = 0
+        Bn1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Bn1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Bn1.BackColor = System.Drawing.Color.Transparent
+
+        'To Set all the Input text or numbers unto the center  
+        Tb1.TextAlign = HorizontalAlignment.Center
+        'It makes the Textbox Layout works perfectly
+        Tb1.Multiline = True
+        'To Set Value on textbox on load to avoid error if Button is clicked while textbox is empty
+        Tb1.Text = 0
+
+        'To layout the position and size of the TextBox Check
+        Tb1.Width = pnlSukatan.Width / 2.65
+        Tb1.Height = pnlSukatan.Height / 6
+        Tb1.Left = pnlSukatan.Left + pnlSukatan.Width / 1.85
+        Tb1.Top = pnlSukatan.Top + pnlSukatan.Height / 2.9
+    End Sub
+
+
 End Class
